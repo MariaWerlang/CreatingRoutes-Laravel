739d480d90183a17214798250c02313bf486fb8c
MariaWerlang <maria.werlang@etec.sp.gov.br>
2023-02-24 20:31:39 -0300

    First File

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1> Home </h1>
+            <p> <a href="/produtos"> Produtos </a> </p>
+            <p> <a href="/empresa"> Empresa </a> </p>
+            <p> <a href="/contato"> Contatos </a> </p>
+    </body>
+
+</html>
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1> Produtos </h1>
+            <p> <a href="/home"> Home </a> </p>
+            <p> <a href="/empresa"> Empresa </a> </p>
+            <p> <a href="/contato"> Contatos </a> </p>
+            
+    </body>
+
+</html>
